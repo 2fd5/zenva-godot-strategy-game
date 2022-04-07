839e4ed3b7e7bc809eff5d6ab6617670a0bcf0ed
Maciej Piatek <magic@2fd5.com>
2022-04-07 21:09:05 +0100

    new: initial tile script
    * variables and basic tile functions
    * Result of Part 6. Tile Script

---
+++ Tile.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+export var startTile : bool = false
+
+var hasBuilding : bool = false
+var canPlaceBuilding : bool = false 
+
+# components
+onready var highlight : Sprite = get_node("Highlight") 
+onready var buildingIcon : Sprite = get_node("BuildingIcon")
+
+func _ready():
+	 add_to_group("Tiles")
+
+func toggle_hightlight(toggle):
+	highlight.visible = toggle
+	canPlaceBuilding = toggle
+ 
+func place_building(buildingTexture):
+	hasBuilding = true
+	buildingIcon.texture = buildingTexture
+ 
+
+func _on_Tile_input_event(viewport, event, shape_idx):
+	pass # Replace with function body. 
